72992edf1ea330564f97024dea5bcecc5e3269d6
Ruben Poppe <ruben.poppe@student.odisee.be>
2021-10-08 17:40:42 +0200

    add detail api queries

--- util/api.ts
+++ util/api.ts
@@ -1,11 +1,11 @@
 import { Recipe } from '../types';
 import { GraphQLClient, gql } from 'graphql-request';
 
-export const getAllRecipes = (): Promise<{ allRecipe: Recipe[] }> => {
-	const client = new GraphQLClient(process.env.CMS_URL, {
-		headers: { Authorization: `Bearer ${process.env.CMS_API_KEY}` },
-	});
+const client = new GraphQLClient(process.env.CMS_URL, {
+	headers: { Authorization: `Bearer ${process.env.CMS_API_KEY}` },
+});
 
+export const getAllRecipes = (): Promise<{ allRecipe: Recipe[] }> => {
 	const query = gql`
 		{
 			allRecipes {
@@ -20,6 +20,49 @@ export const getAllRecipes = (): Promise<{ allRecipe: Recipe[] }> => {
 				}
 				totalTime
 				_publishedAt
+			}
+		}
+	`;
+
+	return client.request(query);
+};
+
+export const getRecipeBySlug = (slug: string): Promise<{ recipe: Recipe }> => {
+	const query = gql`
+		{
+			recipe(filter: { slug: { eq: "${slug}" } }) {
+				title
+				description
+				image {
+					url
+				}
+				vegetarian
+				vegan
+				servings
+				prepTime
+				totalTime
+				ingredients
+				instructions
+				calories
+				fatContent
+				cholesterolContent
+				proteinContent
+				carbohydrateContent
+				_publishedAt
+			}
+		}
+	`;
+
+	return client.request(query);
+};
+
+export const getAllRecipesSlug = (): Promise<{
+	allRecipes: { slug: string }[];
+}> => {
+	const query = gql`
+		{
+			allRecipes {
+				slug
 			}
 		}
 	`;
