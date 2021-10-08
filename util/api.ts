53916ad294362605e0170e0a52241e6edceb00f8
Ruben Poppe <ruben.poppe@student.odisee.be>
2021-10-08 12:35:08 +0200

    add getAll api request

---
+++ util/api.ts
@@ -0,0 +1,27 @@
+import { Recipe } from '../types';
+import { GraphQLClient, gql } from 'graphql-request';
+
+export const getAllRecipes = (): Promise<{ allRecipe: Recipe[] }> => {
+	const client = new GraphQLClient(process.env.CMS_URL, {
+		headers: { Authorization: `Bearer ${process.env.CMS_API_KEY}` },
+	});
+
+	const query = gql`
+		{
+			allRecipes {
+				title
+				slug
+				description
+				vegan
+				vegetarian
+				image {
+					url
+				}
+				totalTime
+				_publishedAt
+			}
+		}
+	`;
+
+	return client.request(query);
+};
