0c415120daa7b11afaa0b5ec7c884e343e63811c
Ruben Poppe <ruben.poppe@student.odisee.be>
2021-10-09 11:20:44 +0200

    switch to preact in prod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	webpack: (config) => {
+	webpack: (config, { dev }) => {
+		if (!dev) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			});
+		}
+
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ['@svgr/webpack'],
