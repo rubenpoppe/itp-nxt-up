1e2ba7b0cb826bbe40a93327b1e10feac001dd40
Ruben Poppe <ruben.poppe@student.odisee.be>
2021-10-09 15:41:13 +0200

    add nav bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,29 @@
 import { FunctionComponent } from 'react';
+import Link from 'next/link';
+import { AppBar, SvgIcon, Toolbar, Typography } from '@mui/material';
+import Icon from '../public/icon.svg';
+
 const Layout: FunctionComponent = ({ children }) => {
 	return (
+		<>
+			<AppBar position="sticky">
+				<Toolbar>
+					<Link href="/">
+						<a>
+							<SvgIcon
+								component={Icon}
+								viewBox="0 0 512 512"
+								aria-labelledby="page-title"
+							/>
+							<Typography variant="h5" component="span" id="page-title">
+								Recipes.com
+							</Typography>
+						</a>
+					</Link>
+				</Toolbar>
+			</AppBar>
 			<main>{children}</main>
+		</>
 	);
 };
 
